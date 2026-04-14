5cc072368c3ef070172479819c99f1a5659f6e5c
Nik-4212 <rajputbattalion854@gmail.com>
2026-04-14 07:40:05 +0000

    Day 6

--- firstproject/my-react-app/src/App.jsx
+++ firstproject/my-react-app/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Demo} from './Component/demo'
-import Demo2 from './Nikhilcomponent/Demo2'
+import{useState} from'react'
+import'./App.css'
 
-function App() {
+function App(){
+  const [data,setCount]=useState("hello")
+  const [isOn, setIsOn]=useState(false);
+  function toggleButton(){
+    setIsOn(!isOn)
+  }
+  function fun(){
+    setCount("bye")
+  }
   return(
-<>
-    <h1>hello this is example of component</h1>
-    <Demo></Demo>
-    <Demo2></Demo2>
-    
-</>
+    <>
+    <h1>
+      State of variable</h1>
+      <h2>{data}</h2>
+      <button onClick={fun}>Change text</button>
+      <h1>toggle Button</h1>
+      <h3>Status:{isOn ? "ON":"OFF"}</h3>
+      <button onClick={toggleButton}>{isOn ? "Turn OFF" : "Turn ON"}</button></>
   )
 }
-
-export default App
+export default App;
